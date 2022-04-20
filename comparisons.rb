62049d86607c0c9176edbd70b377cb0745203d67
Thomas Turner <Thomasturner482@yahoo.com>
2022-04-20 15:45:49 -0500

    test question answers

--- comparisons.rb
+++ comparisons.rb
@@ -53,21 +53,23 @@ puts "Is number_teachers greater than number_students?", number_teachers > numbe
 # Make sure YOU can explain it that way!
 
 puts 4 < 9
-#YOU DO: Explain.
+#YOU DO: true. the code if sking if 4 is les thsn 9.
 
 books = 3
 puts 4 < books
-# YOU DO: Explain.
+# YOU DO: false. books has been set at 3. the code is asking if 4 is less than the value given to books.
 
 friends = 6
 siblings = 2
 puts friends > siblings
-# YOU DO: Explain.
+# YOU DO: true. friends is set to 6 and siblings is set to 2. the code is asking if the value given to
+ friends is greater than the value given to siblings.
 
 attendees = 9;
 meals = 8;
 puts attendees != meals
-# YOU DO: Explain.
+# YOU DO: true.  attendees is set to 9 and meals is set to 8. the code is asking if the value given
+to attendees is not equal to the value given to meals.
 
 
 #-------------------
@@ -90,15 +92,16 @@ age = 1
 
 # YOU DO:
 # Determine if the dog loves to play and loves treats
-
+loves_to_play && loves_treats
 
 # Determine if the dog loves to play and loves the dog park
-
+loves_to_play && loves_dog_park
 
 # Determine if the dog loves to play or loves the dog park
-
+loves_to_play || loves_dog_park
 
 # Determine if the dog loves to play and is a puppy
-
+loves_to_play && age <=1
 # What did your final line of code evaluate to? Why do you think that is? Explain.
-# ANSWER:
+# ANSWER:true. A "and" statement only comes back true if both variables are true. since loves_to_play is true
+and the dog is 1, they both are true so the output is true.
